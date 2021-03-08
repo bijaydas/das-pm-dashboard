17c2e66f3f7293f78d7adcfbaebd602ac6eed2d6
bijaydas <bijay@ligious.com>
2021-03-09 01:36:56 +0530

    Initial project structure

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/tailwind.css'
+
+function app({Component, pageProps}) {
+    return <Component {...pageProps} />
+}
+
+export default app;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+function index() {
+    return (
+        <div>
+            Hello from index.js
+        </div>
+    )
+}
+
+export default index
